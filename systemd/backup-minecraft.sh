a1d5f03de41da9f4c9e3cf51889cce8e6f707890
fargusplumdoodle <fargusplumdoodle@gmail.com>
2019-07-01 10:38:45 -0700

    AUTO-COMMIT--PlumdoodleMCServer-Mon Jul  1 10:38:45 PDT 2019

--- systemd/backup-minecraft.sh
+++ systemd/backup-minecraft.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+echo help me
 
 # this is for backing up a git repository on a daily basis
 
 GIT_DIR=/PlumdoodleMinecraftServer
 NAME=PlumdoodleMCServer
+
 
 # changing directory to git directory
 cd $GIT_DIR
